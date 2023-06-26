2cf7406bf91f95bb175aea270c4064754387b177
Kirandeep2806 <kirandeep102030@gmail.com>
2023-06-27 00:09:55 +0530

    Added code for template rendering

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "I am the Home Page"
+    name = ["Kiran", "Deep", "Patcha"]
+    return render_template("index.html", content=name)
 
 @app.route("/redirect")
 def redirector():
