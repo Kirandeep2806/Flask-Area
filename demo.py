31412e55e8dc567c2a888387117f8c5168737101
Kirandeep2806 <kirandeep102030@gmail.com>
2023-06-27 00:03:15 +0530

    Added redirect, url_for

--- demo.py
+++ demo.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "I am the Home Page"
+
+@app.route("/redirect")
+def redirector():
+    return redirect(url_for("printName", name="Kiran"))
+
+@app.route("/<name>")
+def printName(name):
+    return f"URL has {name}"
 
 if __name__ == "__main__":
     app.run(debug=True)
