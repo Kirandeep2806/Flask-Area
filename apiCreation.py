0219c70cdb0cc1c77d169fd8cadf4694650dc330
Kirandeep2806 <kirandeep102030@gmail.com>
2023-06-27 14:48:31 +0530

    Added API Creation

--- apiCreation.py
+++ apiCreation.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,12 +15,22 @@ data = {"kiran": {"name": {"fname": "Kiran", "lname": "Deep"}, "age": 18, "male"
 
 class Print(Resource):
     def get(self, name):
-        return data[name]
+        if name not in data:
+            abort(404, message="Name don't exist!")
+        return data[name], 200
     
     def put(self, name):
+        if name in data:
+            abort(409, message="Data already exists!")
         args = parser.parse_args()
         data[name] = {"name": {"fname" : args["fname"], "lname": args["lname"]}, "age": args["age"], "male": args["male"]}
-        return data
+        return data, 201
+    
+    def delete(self, name):
+        if name not in data:
+            abort(404, message="Name don't exist!")
+        del data[name]
+        return '', 204
 
 api.add_resource(Print, "/print/<string:name>")
 
